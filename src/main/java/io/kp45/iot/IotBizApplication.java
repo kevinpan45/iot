7823cf5d772967b324b7d346b26c4c2acde77b17
KP45 <kevinpan45@hotmail.com>
2021-01-28 15:18:55 +0800

    init

---
+++ src/main/java/io/kp45/iot/IotBizApplication.java
@@ -0,0 +1,13 @@
+package io.kp45.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotBizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotBizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/kp45/iotbiz/IotBizApplicationTests.java
@@ -0,0 +1,13 @@
+package io.kp45.iotbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotBizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

89433bea7dc0eef063b067ae41220a6bcfec3ebb
KP45 <kevinpan45@hotmail.com>
2021-01-28 20:44:55 +0800

    fix kafka config

--- src/main/java/io/kp45/iot/IotBizApplication.java
+++ src/main/java/io/kp45/iot/IotBizApplication.java
@@ -1,7 +1,9 @@
 package io.kp45.iot;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class IotBizApplication {
@@ -9,5 +11,10 @@ public class IotBizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IotBizApplication.class, args);
 	}
+
+	@KafkaListener(topics = "transportMessageForEvent", groupId = "iot-biz-backend")
+    public void handler(ConsumerRecord<String, String> record) {
+        System.out.println("Recieve data : " + record.value());
+    }
 
 }

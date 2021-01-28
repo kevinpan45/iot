d9e78db886f1631f4df50cf529763458e167ec4f
KP45 <kevinpan45@hotmail.com>
2021-01-28 20:32:09 +0800

    add kafka consumer group

--- src/main/java/io/kp45/iot/mq/AttributeSub.java
+++ src/main/java/io/kp45/iot/mq/AttributeSub.java
@@ -4,7 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 
 public class AttributeSub {
-    @KafkaListener(topics = "transportMessageForEvent")
+    @KafkaListener(topics = "transportMessageForEvent", groupId = "iot-biz-backend")
     public void handler(ConsumerRecord<String, String> record) {
         System.out.println("Recieve data : " + record.value());
     }
